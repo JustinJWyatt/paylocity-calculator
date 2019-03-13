c8d73ea7a3c2d6b3932328dbd5380819825ed5b4
Justin Wyatt <justinjwyatt@gmail.com>
2019-03-13 17:25:59 -0500

    added html components and vue methods

--- pay-calculator/src/components/HelloWorld.vue
+++ pay-calculator/src/components/HelloWorld.vue
@@ -1,42 +1,111 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="hello container">
+
+    <b-form>
+      <b-form-group
+        id="employeeNameGroup"
+        label-cols-sm="4"
+        label-cols-lg="3"
+        label="Employee Name"
+        label-for="employee-name"
+        label-size="sm"
+      >
+        <b-form-input id="employee-name" v-model="employeeName" type="text" placeholder="Enter employee name" />
+      </b-form-group>
+
+      <b-form-group
+        id="dependentGroup"
+        label-cols-sm="4"
+        label-cols-lg="3"
+        label="Dependents"
+        label-size="sm"
+      >
+        <div v-for="d in dependents">
+          <b-form-input class="dependent" type="text" v-model="d.value" placeholder="Add dependent name" />
+        </div>
+      </b-form-group>
+
+      <b-button id="add-dependent" v-on:click="addDependent" variant="outline-primary" :aria-disabled="calculated">Add Dependent</b-button>
+    </b-form>
+
+    <div class="button-container" >
+      <b-button id="calculate" v-if="!calculated" v-on:click="calculate" variant="success">Calculate</b-button>
+      <b-button id="reset" v-if="calculated" v-on:click="reset" variant="success">Reset</b-button>
+    </div>
+
+    <div class="result-container" v-if="calculated">
+      {{employeeName}}'s salary is ${{salary}}. Total deductions are ${{deductions}} with {{dependents.filter(x => x.value).length}} dependents.
+    </div>
   </div>
+
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: "HelloWorld",
+  data: function() {
+    return {
+      deductions: 0,
+      paychecks: 26,
+      employeeName: "",
+      salary: 0,
+      pay: 2000,
+      calculated: false,
+      dependents: [{ value: "" }]
+    };
+  },
+  methods: {
+    addDependent: function() {
+      if (this.dependents.length == 5) {
+        return;
+      }
+      this.dependents.push({ value: "" });
+    },
+    reset: function(){
+      this.calculated = false;
+      this.employeeName = "";
+      this.deductions = 0;
+      this.salary = 0;
+      this.dependents = [{ value: "" }];
+    },
+    calculate: function() {
+
+      if(!this.employeeName){
+        return;
+      }
+
+      //salary is 26 paychecks times pay
+      this.salary = this.paychecks * this.pay;
+
+      //start out with initial deductions for the year
+      this.deductions = 1000;
+
+      //reduce deductions if name starts with a
+      if (this.employeeName.toLowerCase().startsWith("a")) {
+        this.deductions -= 10 / 100 * 1000;
+      }
+
+      if (this.dependents) {
+        this.dependents.forEach(dependent => {
+          //check if value has text and passes regular expression
+          if(dependent.value && /^[a-z ,.'-]+$/i.test(dependent.value)){
+            this.deductions += 500;
+
+            //reduce deductions if name starts with a
+            if (dependent.value.toLowerCase().startsWith("a")) {
+              this.deductions -= 10 / 100 * 500;
+            }
+          }
+        });
+      }
+
+      //subtract all deductions
+      this.salary -= this.deductions;
+
+      this.calculated = true;
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -54,5 +123,36 @@ li {
 }
 a {
   color: #42b983;
+}
+.container {
+  max-width: 550px;
+  border: 1px solid black;
+  height: 550px;
+  border-radius: 15px;
+  position: relative;
+}
+
+#employeeNameGroup {
+  margin-top: 15px;
+}
+
+#add-dependent {
+  float: right;
+}
+
+#calculate, #reset {
+  position: absolute;
+  bottom: 0;
+  margin-bottom: 15px;
+}
+
+#result-container{
+  position: absolute;
+  bottom: 0;
+  margin-bottom: 15px;
+}
+
+.dependent {
+  margin-bottom: 5px;
 }
 </style>
--- pay-calculator/src/main.js
+++ pay-calculator/src/main.js
@@ -4,6 +4,7 @@ import BootstrapVue from 'bootstrap-vue'
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue);
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
